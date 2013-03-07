a37864845a7665dee822c9aeec37ebfc296855a1
Dave Kincaid <kincaid.dave@gmail.com>
2013-03-07 11:15:11 -0600

    Revving version for update to marmalade repo

--- midje-mode-pkg.el
+++ midje-mode-pkg.el
@@ -1,3 +1,3 @@
-(define-package "midje-mode" "0.1.1"
+(define-package "midje-mode" "0.1.3"
   "Minor mode for running Midje tests in emacs, see: https://github.com/dnaumov/midje-mode"
-  '((nrepl "0.1.4") (clojure-mode "1.0")))
+  '((nrepl "0.1.4") (clojure-mode "2.0")))
